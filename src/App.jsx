a10fd2aec72baa49a044f75b8e2cf1923439da5f
om kaldate <omkaldate23@gmail.com>
2025-03-04 22:23:21 +0530

    forms hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const App = ()=>{
-    return(
-        ""
+
+const App = () => {
+
+    const [fullName, setFullName] = useState({
+        fname: "",
+        lname: ""
+    });
+
+    const inputEvent = (e) => {
+
+        setFullName((ele) => {
+            if (e.target.name == "fName") {
+                return {
+                    fname: (e.target.value),
+                    lname: (ele.lname)
+                }
+
+            }
+            if (e.target.name == "lName") {
+                return {
+                    fname: (ele.fname),
+                    lname: (e.target.value)
+                }
+            }
+        })
+    };
+
+    const clicked = (e) => {
+        e.preventDefault();
+        alert("form submitted");
+
+    }
+
+    return (
+        <>
+            <form onSubmit={clicked}>
+                <div>
+                    <h2> Hello {fullName.fname} {fullName.lname}</h2>
+                    <input type="text" placeholder="Enter Yout name" onChange={inputEvent} name="fName" value={fullName.fname} />
+                    <input type="text" placeholder="Enter Yout last name" onChange={inputEvent} name="lName" value={fullName.lname} />
+                    <button placeholder="click me" type="submit"></button>
+                </div>
+            </form>
+
+        </>
     )
 }
 
