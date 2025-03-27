c61ccd64725c6c5885bf7760e1583dfc3dbc0663
om kaldate <omkaldate23@gmail.com>
2025-03-27 23:59:11 +0530

    raect

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,51 @@
-import React, { useState } from "react";
-import AcUnitIcon from '@mui/icons-material/AcUnit';
+import React from "react";
+import Menu from "./Menu";
+import {Rout, Switch} from "react-router-dom";
 
 const App = () => {
 
-    const [fullName, setFullName] = useState({
-        fname: "",
-        lname: ""
-    });
 
-    const inputEvent = (e) => {
-        const {name, value} = e.target;
+    <Menu/>
 
-        setFullName((ele) => {
-            return {
-                ...fullName,
-                [name]: value
-            }
 
-        })
-    };
 
-    const clicked = (e) => {
-        e.preventDefault();
-        alert("form submitted");
+    // const [fullName, setFullName] = useState({
+    //     fname: "",
+    //     lname: ""
+    // });
 
-    }
+    // const inputEvent = (e) => {
+    //     const {name, value} = e.target;
 
-    return (
-        <>
-            <form onSubmit={clicked}>
-                <div>
-                    <h2> Hello {fullName.fname} {fullName.lname}</h2>
-                    <input type="text" placeholder="Enter Yout name" onChange={inputEvent} name="fname" value={fullName.fname} />
-                    <input type="text" placeholder="Enter Yout last name" onChange={inputEvent} name="lname" value={fullName.lname} />
-                    <button placeholder="click me" type="submit"></button>
-                </div>
-                <AcUnitIcon />
-            </form>
+    //     setFullName((ele) => {
+    //         return {
+    //             ...fullName,
+    //             [name]: value
+    //         }
 
-        </>
-    )
+    //     })
+    // };
+
+    // const clicked = (e) => {
+    //     e.preventDefault();
+    //     alert("form submitted");
+
+    // }
+
+    // return (
+    //     <>
+    //         <form onSubmit={clicked}>
+    //             <div>
+    //                 <h2> Hello {fullName.fname} {fullName.lname}</h2>
+    //                 <input type="text" placeholder="Enter Yout name" onChange={inputEvent} name="fname" value={fullName.fname} />
+    //                 <input type="text" placeholder="Enter Yout last name" onChange={inputEvent} name="lname" value={fullName.lname} />
+    //                 <button placeholder="click me" type="submit"></button>
+    //             </div>
+    //             <AcUnitIcon />
+    //         </form>
+
+    //     </>
+    // )
 }
 
 export default App;
---
+++ src/Menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+    return (
+        <>
+            <h2>Hellooooo</h2>
+            <Link to="/">About Us</Link>
+            <Link to="/component">Contact Us</Link>
+        </>
+    );
+};
+
+export default Menu;
